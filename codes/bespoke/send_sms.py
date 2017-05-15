48ea2c4fdd197ce1b113d7b88eecb504803ce1cc
Ojewale Olusegun <Ojewale.Olusegun@ynap.com>
2017-05-15 23:58:21 +0100

    updated for day 2

--- codes/bespoke/send_sms.py
+++ codes/bespoke/send_sms.py
@@ -5,8 +5,8 @@ from twilio.rest import TwilioRestClient
 class SMS_UTILS ():
 # Find these values at https://twilio.com/user/account
 	def send_sms(self,to_number, msg):
-		account_sid = "AC0ac5b24e4efc346356"
-		auth_token = "b3c7ed9b6bf5713740"
+		account_sid = "AC0a4efc346356"
+		auth_token = "b3c7e713740"
 		client = TwilioRestClient(account_sid, auth_token)
 	
 		message = client.messages.create(to=to_number, from_="+44",body=msg)
